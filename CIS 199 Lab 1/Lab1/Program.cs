43e30b7622021ff6ceca86be2a65f0a8d99983f2
MattLanc <149915841+MattLanc@users.noreply.github.com>
2023-11-05 01:07:10 -0500

    Add files via upload

---
+++ CIS 199 Lab 1/Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Grading ID: S5027
+// Lab Number: Lab 1
+// Due Date:   09/05/2021
+// Course:     CIS 199-50
+
+// This propgram outputs the Grading ID, Hobbies, Favorite Book, and Favorite Movie
+// To the console where it is displayed
+
+
+using static System.Console;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Grading ID:     S5027");
+            WriteLine("Hobbies:        Golfing and Reading");
+            WriteLine("Favorite Book:  Bleach");
+            WriteLine("Favorite Movie: The Da Vinci Code");
+            
+            // I didnt have to use "Console." when using the WriteLine because I used
+            // "using static System.Console"
+        }
+    }
+}
